a4087a9c548325ceef904a4ae39be70e3bb958a1
Ravindra Kumar Kushwaha <47721333+ravindra1234567@users.noreply.github.com>
2021-09-27 16:19:13 +0530

    Update App.java

--- src/main/java/com/nagarro/javatraining/maven/App.java
+++ src/main/java/com/nagarro/javatraining/maven/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Second Hello World!" );
+        System.out.println( "Third Hello World!" );
         
     }
 }
